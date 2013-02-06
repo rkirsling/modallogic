5339bafd9ae74a49b75336d91493221076ef700c
Ross Kirsling <rkirsling@gmail.com>
2013-02-05 22:53:59 -0600

    First commit
    Done: MPL, D3.
    Not done: Angular, documentation, etc.

---
+++ js/MPL.js
@@ -0,0 +1,180 @@
+/**
+ * MPL: A library for parsing and evaluating modal propositional logic.
+ *
+ * @author  Ross Kirsling
+ * @date    2012.01.28
+ * @version 1.0
+ */
+
+var MPL = (function() {
+  'use strict';
+
+  // sub-regexes
+  var beginPart         = '^\\(',
+      unaryPart         = '(?:~|\\[\\]|<>)',
+      propOrBinaryPart  = '(?:\\w+|\\(.*\\))',
+      subwffPart        = unaryPart + '*' + propOrBinaryPart,
+      endPart           = '\\)$';
+
+  // binary operator regexes
+  var conjRegEx = new RegExp(beginPart + '(' + subwffPart + ')&('   + subwffPart + ')' + endPart), // (p&q)
+      disjRegEx = new RegExp(beginPart + '(' + subwffPart + ')\\|(' + subwffPart + ')' + endPart), // (p|q)
+      implRegEx = new RegExp(beginPart + '(' + subwffPart + ')->('  + subwffPart + ')' + endPart), // (p->q)
+      equiRegEx = new RegExp(beginPart + '(' + subwffPart + ')<->(' + subwffPart + ')' + endPart); // (p<->q)
+
+  // proposition regex
+  var propRegEx = /^\w+$/;
+
+  // regex to check if wff DOESN'T need outer parentheses;
+  // i.e., wff is prop, is parenthesized, or starts with unary operator
+  var parenCheckRegEx = new RegExp('^' + propOrBinaryPart + '$|^' + unaryPart);
+
+  /**
+   * Helper function for removing all whitespace from a string.
+   * @private
+   */
+  function removeWhitespace(str) {
+    return str.match(/\S+/g).join('');
+  }
+
+  /**
+   * Converts MPL wff string to JSON object.
+   * @private
+   */
+  function _wffToJSON(wff) {
+    var json    = {},
+        subwffs = [];
+
+    if(propRegEx.test(wff))
+      json.prop = wff;
+    else if(wff.charAt(0) === '~')
+      json.neg = _wffToJSON(wff.slice(1));
+    else if(wff.substr(0, 2) === '[]')
+      json.nec = _wffToJSON(wff.slice(2));
+    else if(wff.substr(0, 2) === '<>')
+      json.poss = _wffToJSON(wff.slice(2));
+    else if(subwffs = wff.match(conjRegEx))
+      json.conj = [_wffToJSON(subwffs[1]), _wffToJSON(subwffs[2])];
+    else if(subwffs = wff.match(disjRegEx))
+      json.disj = [_wffToJSON(subwffs[1]), _wffToJSON(subwffs[2])];
+    else if(subwffs = wff.match(implRegEx))
+      json.impl = [_wffToJSON(subwffs[1]), _wffToJSON(subwffs[2])];
+    else if(subwffs = wff.match(equiRegEx))
+      json.equi = [_wffToJSON(subwffs[1]), _wffToJSON(subwffs[2])];
+    else
+      throw new Error('invalid wff!');
+
+    return json;
+  }
+
+  /**
+   * Preprocesses MPL wff string, then converts to JSON object using previous function.
+   */
+  function wffToJSON(wff) {
+    wff = removeWhitespace(wff);
+    if(!parenCheckRegEx.test(wff)) wff = '(' + wff + ')';
+
+    return _wffToJSON(wff);
+  }
+
+  /**
+   * Converts the JSON representation of an MPL wff to its string representation.
+   */
+  function jsonToWff(json) {
+    if(json.prop)
+      return json.prop;
+    else if(json.neg)
+      return '~' + jsonToWff(json.neg);
+    else if(json.nec)
+      return '[]' + jsonToWff(json.nec);
+    else if(json.poss)
+      return '<>' + jsonToWff(json.poss);
+    else if(json.conj && json.conj.length === 2)
+      return '(' + jsonToWff(json.conj[0]) + ' & ' + jsonToWff(json.conj[1]) + ')';
+    else if(json.disj && json.disj.length === 2)
+      return '(' + jsonToWff(json.disj[0]) + ' | ' + jsonToWff(json.disj[1]) + ')';
+    else if(json.impl && json.impl.length === 2)
+      return '(' + jsonToWff(json.impl[0]) + ' -> ' + jsonToWff(json.impl[1]) + ')';
+    else if(json.equi && json.equi.length === 2)
+      return '(' + jsonToWff(json.equi[0]) + ' <-> ' + jsonToWff(json.equi[1]) + ')';
+    else
+      throw new Error('invalid JSON for formula!');
+  }
+
+  /**
+   * Converts an MPL wff string to a LaTeX expression.
+   */
+  function wffToLaTeX(wff) {
+    return wff.replace(/~/g,    '\\lnot{}')
+              .replace(/\[\]/g, '\\Box{}')
+              .replace(/<>/g,   '\\Diamond{}')
+              .replace(/&/g,    '\\land{}')
+              .replace(/\|/g,   '\\lor{}')
+              .replace(/<->/g,  '\\leftrightarrow{}')
+              .replace(/->/g,   '\\rightarrow{}');
+  }
+
+  /**
+   * Converts the JSON representation of an MPL wff to a LaTeX expression.
+   */
+  function jsonToLaTeX(json) {
+    return wffToLaTeX(jsonToWff(json));
+  }
+
+  /**
+   * Constructor for Kripke model.
+   * @constructor
+   */
+  function Model(propvars, states, relation) {
+    // array of names of propositional variables in use
+    this.propvars = propvars;
+
+    // array of states in model, where each state is represented by a truth assignment;
+    // i.e., an array of booleans corresponding to propvars
+    this.states = states;
+
+    // accessibility relation represented as array of arrays;
+    // one subarray for each state in model, which is a list of successor state indices
+    this.relation = relation;
+    
+    // returns truth value for a given proposition at a given state in the model
+    this.valuation = function(prop, state) {
+      return this.states[state][this.propvars.indexOf(prop)];
+    };
+  }
+
+  /**
+   * Evaluate truth of a wff at a given state within a given model.
+   */
+  function truth(model, state, wff) {
+    if(wff.prop)
+      return model.valuation(wff.prop, state);
+    else if(wff.neg)
+      return !truth(model, state, wff.neg);
+    else if(wff.conj)
+      return wff.conj.every(function(subwff) { return truth(model, state, subwff); });
+    else if(wff.disj)
+      return wff.disj.some(function(subwff) { return truth(model, state, subwff); });
+    else if(wff.impl)
+      return (!truth(model, state, wff.impl[0]) || truth(model, state, wff.impl[1]));
+    else if(wff.equi)
+      return (truth(model, state, wff.equi[0]) === truth(model, state, wff.equi[1]));
+    else if(wff.nec)
+      return model.relation[state].every(function(succState) { return truth(model, succState, wff.nec) });
+    else if(wff.poss)
+      return model.relation[state].some(function(succState) { return truth(model, succState, wff.poss) });
+    else
+      throw new Error('invalid formula!');
+  }
+
+  // export public methods
+  return {
+    wffToJSON: wffToJSON,
+    jsonToWff: jsonToWff,
+    wffToLaTeX: wffToLaTeX,
+    jsonToLaTeX: jsonToLaTeX,
+    Model: Model,
+    truth: truth
+  };
+
+})();
---
+++ js/app.js
@@ -0,0 +1,419 @@
+/**
+ * Modal Logic Playground -- application code
+ * D3 directed graph code based largely on the tutorials at https://github.com/mbostock/d3/wiki/Gallery
+ */
+
+// app mode constants
+var MODE = {
+      EDIT: 0,
+      VIEW: 1
+    },
+    appMode = MODE.EDIT;
+
+// set up SVG for D3
+var width  = 640,//960,
+    height = 540,
+    colors = d3.scale.category10();
+
+var svg = d3.select('#app-container .graph')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+// set up initial nodes and links
+var nodes = [
+      {id: 0, vals: [false, false], reflexive: false},
+      {id: 1, vals: [false, true ], reflexive: false},
+      {id: 2, vals: [true,  false], reflexive: false},
+      {id: 3, vals: [true,  true ], reflexive: true }
+    ],
+    lastNodeId = 3,
+    links = [
+      {source: nodes[0], target: nodes[1], left: true,  right: true },
+      {source: nodes[0], target: nodes[2], left: false, right: true },
+      {source: nodes[1], target: nodes[3], left: true,  right: false},
+      {source: nodes[2], target: nodes[3], left: false, right: true }
+    ];
+
+// init D3 force layout
+var force = d3.layout.force()
+    .nodes(nodes)
+    .links(links)
+    .size([width, height])
+    .linkDistance(150)
+    .charge(-500)
+    .on('tick', tick)
+
+// define arrow markers for graph links
+svg.append('svg:defs').append('svg:marker')
+    .attr('id', 'end-arrow')
+    .attr('viewBox', '0 -5 10 10')
+    .attr('refX', 6)
+    .attr('markerWidth', 3)
+    .attr('markerHeight', 3)
+    .attr('orient', 'auto')
+  .append('svg:path')
+    .attr('d', 'M0,-5L10,0L0,5')
+    .attr('fill', '#000');
+
+svg.append('svg:defs').append('svg:marker')
+    .attr('id', 'start-arrow')
+    .attr('viewBox', '0 -5 10 10')
+    .attr('refX', 4)
+    .attr('markerWidth', 3)
+    .attr('markerHeight', 3)
+    .attr('orient', 'auto')
+  .append('svg:path')
+    .attr('d', 'M10,-5L0,0L10,5')
+    .attr('fill', '#000');
+
+// line displayed when dragging new nodes
+var drag_line = svg.append('svg:path')
+  .attr('class', 'link hidden')
+  .attr('d', 'M0,0L0,0');
+
+// handles to node, edge, and label element groups
+var path = svg.append('svg:g').selectAll('path'),
+    circle = svg.append('svg:g').selectAll('g');
+
+// mouse event vars
+var selected_node = null,
+    selected_link = null,
+    mousedown_link = null,
+    mousedown_node = null,
+    mouseup_node = null;
+
+function resetMouseVars() {
+  mousedown_node = null;
+  mouseup_node = null;
+  mousedown_link = null;
+}
+
+
+// update force layout (called automatically each iteration)
+function tick() {
+  // draw directed edges with proper padding from node centers
+  path.attr('d', function(d) {
+    var deltaX = d.target.x - d.source.x,
+        deltaY = d.target.y - d.source.y,
+        dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
+        normX = deltaX / dist,
+        normY = deltaY / dist,
+        sourcePadding = d.left ? 17 : 12,
+        targetPadding = d.right ? 17 : 12,
+        sourceX = d.source.x + (sourcePadding * normX),
+        sourceY = d.source.y + (sourcePadding * normY),
+        targetX = d.target.x - (targetPadding * normX),
+        targetY = d.target.y - (targetPadding * normY);
+    return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY;
+  });
+
+  circle.attr('transform', function(d) {
+    return 'translate(' + d.x + ',' + d.y + ')';
+  });
+}
+
+// update graph (called when needed)
+function restart() {
+  // path (link) group
+  path = path.data(links);
+
+  // update existing links
+  path.classed('selected', function(d) { return d === selected_link; })
+    .attr('marker-start', function(d) { return d.left ? 'url(#start-arrow)' : ''; })
+    .attr('marker-end', function(d) { return d.right ? 'url(#end-arrow)' : ''; });
+
+  // add new links
+  path.enter().append('svg:path')
+    .attr('class', 'link')
+    .classed('selected', function(d) { return d === selected_link; })
+    .attr('marker-start', function(d) { return d.left ? 'url(#start-arrow)' : ''; })
+    .attr('marker-end', function(d) { return d.right ? 'url(#end-arrow)' : ''; })
+    .on('mousedown', function(d) {
+      if(appMode !== MODE.EDIT) return;
+
+      // select link
+      mousedown_link = d;
+      if(mousedown_link === selected_link) selected_link = null;
+      else selected_link = mousedown_link;
+      selected_node = null;
+      restart();
+    });
+
+  // remove old links
+  path.exit().remove();
+
+
+  // circle (node) group
+  // NB: the function arg is crucial here! nodes are known by id, not by index!
+  circle = circle.data(nodes, function(d){ return d.id; });
+
+  // update existing nodes (reflexive & selected visual states)
+  circle.selectAll('circle')
+    .style('fill', function(d) { return (d === selected_node) ? d3.rgb(colors(d.id)).brighter().toString() : colors(d.id); })
+    .classed('reflexive', function(d) { return d.reflexive; });
+
+  // add new nodes
+  var g = circle.enter().append('svg:g');//.style('opacity',0);
+  //g.transition().style('opacity',1);
+
+  g.append('svg:circle')
+    .attr('class', 'node')
+    .attr('r', 12)
+    .style('fill', function(d) { return (d === selected_node) ? d3.rgb(colors(d.id)).brighter().toString() : colors(d.id); })
+    .style('stroke', function(d) { return d3.rgb(colors(d.id)).darker().toString(); })
+    .classed('reflexive', function(d) { return d.reflexive; })
+    .on('mousedown', function(d) {
+      // select node
+      mousedown_node = d;
+      if(mousedown_node === selected_node) selected_node = null;
+      else selected_node = mousedown_node;
+      selected_link = null;
+
+      if(appMode === MODE.EDIT) {
+        // reposition drag line
+        drag_line
+          .attr('marker-end', 'url(#end-arrow)')
+          .classed('hidden', false)
+          .attr('d', 'M' + mousedown_node.x + ',' + mousedown_node.y + 'L' + mousedown_node.x + ',' + mousedown_node.y);
+      }
+
+      restart();
+    })
+    .on('mouseup', function(d) {
+      if(appMode !== MODE.EDIT || !mousedown_node) return;
+
+      // needed?
+      drag_line
+        .attr('marker-end', '')
+        .classed('hidden', true);
+
+      // check for drag-to-self
+      mouseup_node = d;
+      if(mouseup_node === mousedown_node) { resetMouseVars(); return; }
+
+      // add link (update if exists)
+      // note: links are strictly source < target; arrows separately specified by booleans
+      var source, target, direction;
+      if(mousedown_node.id < mouseup_node.id) {
+        source = mousedown_node;
+        target = mouseup_node;
+        direction = 'right';
+      } else {
+        source = mouseup_node;
+        target = mousedown_node;
+        direction = 'left';
+      }
+
+      var link;
+      link = links.filter(function(l) {
+        return (l.source === source && l.target === target);
+      })[0];
+
+      if(link) {
+        link[direction] = true;
+      } else {
+        link = {source: source, target: target, left: false, right: false};
+        link[direction] = true;
+        links.push(link);
+      }
+
+      // select new link
+      selected_link = link;
+      selected_node = null;
+      restart();
+    })
+
+  // show node IDs
+  g.append('svg:text')
+      .attr('x', 0)
+      .attr('y', 4)
+      .attr('class', 'id')
+      .text(function(d) { return d.id; });
+
+  // text shadow
+  g.append('svg:text')
+      .attr('x', 16)
+      .attr('y', 4)
+      .attr('class', 'shadow')
+      .text(function(d) { return d.vals.join(); });
+
+  // text foreground
+  g.append('svg:text')
+      .attr('x', 16)
+      .attr('y', 4)
+      .text(function(d) { return d.vals.join(); });
+
+  // remove old nodes
+  circle.exit().remove();
+
+  // set the graph in motion
+  force.start();
+}
+
+
+function mousedown() {
+  if(mousedown_node || mousedown_link) return;
+
+  // insert new state at point
+  var point = d3.mouse(this),
+      node = {id: ++lastNodeId, vals: [false,false], reflexive: false};
+  node.x = point[0];
+  node.y = point[1];
+  nodes.push(node);
+
+  restart();
+}
+
+function mousemove() {
+  if (!mousedown_node) return;
+
+  // update drag line
+  drag_line.attr('d', 'M' + mousedown_node.x + ',' + mousedown_node.y + 'L' + d3.mouse(this)[0] + ',' + d3.mouse(this)[1]);
+
+  restart();
+}
+
+function mouseup() {
+  if (mousedown_node) {
+    // hide drag line
+    drag_line.classed('hidden', true)
+      .attr('marker-end', '');
+  }
+  // clear mouse event vars
+  resetMouseVars();
+}
+
+function spliceLinksForNode(node) {
+  var toSplice = links.filter(function(l) {
+    return (l.source === node || l.target === node);
+  });
+  toSplice.map(function(l) {
+    links.splice(links.indexOf(l), 1);
+  });
+}
+
+function keydown() {
+  if(!selected_node && !selected_link) return;
+  switch(d3.event.keyCode) {
+    case 46: // delete
+      if(selected_node) {
+        nodes.splice(nodes.indexOf(selected_node), 1);
+        spliceLinksForNode(selected_node);
+      } else if(selected_link) {
+        links.splice(links.indexOf(selected_link), 1);
+      }
+      selected_link = null;
+      selected_node = null;
+      restart();
+      break;
+    case 66: // B
+      if(selected_link) {
+        // set link direction to both left and right
+        selected_link.left = true;
+        selected_link.right = true;
+      }
+      restart();
+      break;
+    case 76: // L
+      if(selected_link) {
+        // set link direction to left only
+        selected_link.left = true;
+        selected_link.right = false;
+      }
+      restart();
+      break;
+    case 82: // R
+      if(selected_node) {
+        // toggle node reflexivity
+        selected_node.reflexive = !selected_node.reflexive;
+      } else if(selected_link) {
+        // set link direction to right only
+        selected_link.left = false;
+        selected_link.right = true;
+      }
+      restart();
+      break;
+  }
+}
+
+
+function setAppMode(v) {
+  if(v === MODE.EDIT) {
+    svg.on('mousedown', mousedown)
+      .on('mousemove', mousemove)
+      .on('mouseup', mouseup);
+    d3.select(window)
+      .on('keydown', keydown);
+
+    // see: https://groups.google.com/forum/?fromgroups=#!topic/d3-js/-HcNN1deSow
+    circle
+      .on('mousedown.drag', null)
+      .on('touchstart.drag', null);
+
+    //TEMP
+    $('#app-container .panel').html(panelText[0]);
+
+    appMode = MODE.EDIT;
+  } else if(v === MODE.VIEW) {
+    svg.on('mousedown', null)
+      .on('mousemove', null)
+      .on('mouseup', null);
+    d3.select(window)
+      .on('keydown', null);
+
+    circle.call(force.drag);
+
+    // needed?
+    drag_line
+      .attr('marker-end', '')
+      .classed('hidden', true);
+
+    //TEMP
+    $('#app-container .panel').html(panelText[1]);
+
+    appMode = MODE.VIEW;
+  }
+
+  // needed?
+  selected_link = null;
+  selected_node = null;
+  resetMouseVars();
+
+  restart();
+}
+
+// TEMP
+var panelText = [
+  '<ul class="unstyled">' +
+    '<li>Click in the open space to <strong>add a state</strong></li>' +
+    '<br>' +
+    '<li>Drag between states to <strong>add a transition</strong></li>' +
+    '<br>' +
+    '<li>Click a state or a transition to <strong>select</strong> it</li>' +
+    '<br>' +
+    '<li>' +
+      'When a state is selected:' +
+      '<ul>' +
+        '<li><strong>R</strong> toggles reflexivity</li>' +
+        '<li><strong>Delete</strong> removes the state</li>' +
+      '</ul>' +
+    '</li>' +
+    '<br>' +
+    '<li>' +
+      'When a transition is selected:' +
+      '<ul>' +
+        '<li><strong>L</strong>(eft), <strong>R</strong>(ight), <strong>B</strong>(oth) change direction</li>' +
+        '<li><strong>Delete</strong> removes the transition</li>' +
+      '</ul>' +
+    '</li>' +
+  '</ul>',
+  '<ul class="unstyled">' +
+    '<li>Click a state or a transition to <strong>select</strong> it</li>' +
+    '<br>' +
+    '<li>Drag states to <strong>move</strong> the graph layout</li>' +
+  '</ul>'
+];
+
+// app starts here
+setAppMode(MODE.EDIT);
